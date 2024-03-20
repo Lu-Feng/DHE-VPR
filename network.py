5c8e92e70e972fbf8f4046f37ec83660acc513df
Feng Lu <1600925872@qq.com>
2024-03-20 11:22:59 +0800

    Improved readability

--- network.py
+++ network.py
@@ -23,98 +23,6 @@ from transformers import DeiTModel
 import datetime
 from model.cct.cct import cct_14_7x2_384
 
-def drop_path(x, drop_prob: float = 0., training: bool = False):
-    """
-    Obtained from: github.com:rwightman/pytorch-image-models
-    Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
-    This is the same as the DropConnect impl I created for EfficientNet, etc networks, however,
-    the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
-    See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for
-    changing the layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use
-    'survival rate' as the argument.
-    """
-    if drop_prob == 0. or not training:
-        return x
-    keep_prob = 1 - drop_prob
-    shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
-    random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
-    random_tensor.floor_()  # binarize
-    output = x.div(keep_prob) * random_tensor
-    return output
-
-
-class DropPath(nn.Module):
-    """
-    Obtained from: github.com:rwightman/pytorch-image-models
-    Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks).
-    """
-
-    def __init__(self, drop_prob=None):
-        super(DropPath, self).__init__()
-        self.drop_prob = drop_prob
-
-    def forward(self, x):
-        return drop_path(x, self.drop_prob, self.training)
-
-class Attention(Module):
-    """
-    Obtained from timm: github.com:rwightman/pytorch-image-models
-    """
-
-    def __init__(self, dim, num_heads=8, attention_dropout=0.1, projection_dropout=0.1):
-        super().__init__()
-        self.num_heads = num_heads
-        head_dim = dim // self.num_heads
-        self.scale = head_dim ** -0.5
-
-        self.qkv = Linear(dim, dim * 3, bias=False)
-        self.attn_drop = Dropout(attention_dropout)
-        self.proj = Linear(dim, dim)
-        self.proj_drop = Dropout(projection_dropout)
-
-    def forward(self, x):
-        B, N, C = x.shape
-        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
-        q, k, v = qkv[0], qkv[1], qkv[2]
-
-        attn = (q @ k.transpose(-2, -1)) * self.scale
-        attn = attn.softmax(dim=-1)
-        attn = self.attn_drop(attn)
-
-        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
-        x = self.proj(x)
-        x = self.proj_drop(x)
-        return x
-
-class TransformerEncoderLayer(Module):
-    """
-    Inspired by torch.nn.TransformerEncoderLayer and timm.
-    """
-
-    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1,
-                 attention_dropout=0.1, drop_path_rate=0.1):
-        super(TransformerEncoderLayer, self).__init__()
-        self.pre_norm = LayerNorm(d_model)
-        self.self_attn = Attention(dim=d_model, num_heads=nhead,
-                                   attention_dropout=attention_dropout, projection_dropout=dropout)
-
-        self.linear1 = Linear(d_model, dim_feedforward)
-        self.dropout1 = Dropout(dropout)
-        self.norm1 = LayerNorm(d_model)
-        self.linear2 = Linear(dim_feedforward, d_model)
-        self.dropout2 = Dropout(dropout)
-
-        self.drop_path = DropPath(drop_path_rate) if drop_path_rate > 0 else Identity()
-
-        self.activation = F.gelu
-
-    def forward(self, src: torch.Tensor, *args, **kwargs) -> torch.Tensor:
-        src = src + self.drop_path(self.self_attn(self.pre_norm(src)))
-        src = self.norm1(src)
-        src2 = self.linear2(self.dropout1(self.activation(self.linear1(src))))
-        src = src + self.drop_path(self.dropout2(src2))
-        return src
-
 class GeM(nn.Module):
     def __init__(self, p=3, eps=1e-6):
         super().__init__()
@@ -175,23 +83,13 @@ class FeaturesExtractor(torch.nn.Module):
         else:
             raise ValueError(f"Invalid features type: {f_type}")
 
-
-def compute_similarity(features_a, features_b):
-    b, c, h, w = features_a.shape
-    features_a = features_a.transpose(2, 3).contiguous().view(b, c, h*w)
-    features_b = features_b.view(b, c, h*w).transpose(1, 2)
-    features_mul = torch.bmm(features_b, features_a)
-    correlation_tensor = features_mul.view(b, h, w, h*w).transpose(2, 3).transpose(1, 2)
-    correlation_tensor = F.normalize(F.relu(correlation_tensor), p=2, dim=1)
-    return correlation_tensor
-
 class HomographyRegression(nn.Module):
     def __init__(self, output_dim=16):
         super().__init__()
         # self.pre_norm = LayerNorm(576)
-        encoder1 = nn.TransformerEncoderLayer(d_model=576, nhead=12, dim_feedforward=2048, activation="gelu", dropout=0.)
-        self.encoder = nn.TransformerEncoder(encoder1, num_layers=3)
-        self.encoder2 = nn.TransformerEncoder(encoder1, num_layers=3)
+        encoder_layer = nn.TransformerEncoderLayer(d_model=576, nhead=12, dim_feedforward=2048, activation="gelu", dropout=0.)
+        self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=3)
+        self.encoder2 = nn.TransformerEncoder(encoder_layer, num_layers=3)
         self.pe = nn.Parameter(torch.zeros(1, 576, 576),requires_grad=True)
         output_dim_last_conv = 576*576
         self.linear = nn.Linear(output_dim_last_conv, output_dim)
@@ -209,6 +107,14 @@ class HomographyRegression(nn.Module):
         x = self.linear(x)
         return x.reshape(B, 8, 2)
 
+def compute_similarity(features_a, features_b):
+    b, c, h, w = features_a.shape
+    features_a = features_a.transpose(2, 3).contiguous().view(b, c, h*w)
+    features_b = features_b.view(b, c, h*w).transpose(1, 2)
+    features_mul = torch.bmm(features_b, features_a)
+    correlation_tensor = features_mul.view(b, h, w, h*w).transpose(2, 3).transpose(1, 2)
+    correlation_tensor = F.normalize(F.relu(correlation_tensor), p=2, dim=1)
+    return correlation_tensor
 
 class Network(nn.Module):
     """
